80fae12743bcb10e9d6523e9a5a866abf7ff37d7
Henok Dereje <henokdereje061@gmail.com>
2025-09-19 21:36:52 +0300

    Corrected commit for simple calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,18 +1,18 @@
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 operator = input("Choose the operator (+, -, *, /): ")
 result = None
 match operator:
     case "+":
-        result = number1 + number2
+        result = num1 + num2
     case "-":
-        result = number1 - number2
+        result = num1 - num2
     case "*":
-        result = number1 * number2
+        result = num1 * num2
     case "/":
-        if number2 == 0:
+        if num2 == 0:
             print("Cannot divide by zero.")
         else:     
-            result = number1 / number2
+            result = num1 / num2
 if result:
     print(f"The result is {result}")
