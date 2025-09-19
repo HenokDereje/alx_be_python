e3c4c83f1f3b7a818e9a974e01ea3f8083dfc7cb
Henok Dereje <henokdereje061@gmail.com>
2025-09-19 22:02:15 +0300

    Commit for drawing pattern

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+size = int(input("Enter the size of the pattern: "))
+if size>0:
+    row = 0
+    while row<size:
+        for i in range(size):
+            print("*", end="")
+        print()
+        row+=1
+else:
+    print("Enter a positive number.")
