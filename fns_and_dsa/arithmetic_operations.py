71ee138ef26382a0d50d84429dad72849cc11bf2
Henok Dereje <henokdereje061@gmail.com>
2025-09-24 10:07:25 +0300

    A corrected commit for perform operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 def perform_operation(num1, num2, operation):
     result = None
-    match operation:
-        case "add":
-            result = num1 + num2
-        case "subtract":
-            result = num1 - num2
-        case "multiply":
-            result = num1 * num2
-        case "divide":
-            if num2 != 0:
-                result = num1 / num2
-            else:
-                return "You cannot divide by Zero!"
+    if operation == "add":
+        result = num1 + num2
+    if operation == "subtract":
+        result = num1 - num2
+    if operation == "multiply":
+        result = num1 * num2
+    if operation == "divide":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "You cannot divide by Zero!"
     return result
