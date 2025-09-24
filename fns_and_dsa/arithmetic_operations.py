400a6ca6701df84795525b4d0693e67f43ec745b
Henok Dereje <henokdereje061@gmail.com>
2025-09-24 10:09:39 +0300

    A third commit for operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,13 +2,13 @@ def perform_operation(num1, num2, operation):
     result = None
     if operation == "add":
         result = num1 + num2
-    if operation == "subtract":
+    elif operation == "subtract":
         result = num1 - num2
-    if operation == "multiply":
+    elif operation == "multiply":
         result = num1 * num2
-    if operation == "divide":
-        if num2 != 0:
-            result = num1 / num2
-        else:
+    elif operation == "divide":
+        if num2 == 0:
             return "You cannot divide by Zero!"
+        else:
+            result = num1 / num2
     return result
