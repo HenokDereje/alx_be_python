ef0dbe9cbca89bb8e4d78b89129b85c96c629f35
Henok Dereje <henokdereje061@gmail.com>
2025-09-24 10:32:47 +0300

    A commit for the shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,33 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("What item do you want to add?")
+            shopping_list.append(item)
+        elif choice == '2':
+            if shopping_list:
+                item = input("What item do you want to remove?")
+                shopping_list.remove(item)
+            else:
+                print("There is no item in shopping list!")
+        elif choice == '3':
+            print("Your shopping lists:")
+            for shopping_item in shopping_list:print(shopping_item)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
